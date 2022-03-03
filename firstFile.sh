02dc89d36738ca116e8b99a5c99c724ad56a47d5
Mayank <smayank748@gmail.com>
2022-03-03 09:08:06 +0530

    [Mayank]updated the first file

--- firstFile.sh
+++ firstFile.sh
@@ -1,3 +1,21 @@
-echo "Hello world"
-echo "From git hub"
-echo "from first brach"
+#! /bin/bash -x
+
+yourName="Mayank"
+echo $yourName
+
+#loop in shell script
+for((i=0;i<10;i++))
+do
+#body of the loop
+echo $i
+done
+
+#while loop
+echo "=============*************============="
+num=10
+while [ $num -gt 0 ]
+do
+	#body of the loop
+	echo $num
+	((num--))
+done
