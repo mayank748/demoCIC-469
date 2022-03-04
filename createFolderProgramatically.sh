da404541fec45ffdf930edcb5514a782de4ee267
Mayank <smayank748@gmail.com>
2022-03-04 08:45:50 +0530

    [Mayank]added a check for if directory exist or not

--- createFolderProgramatically.sh
+++ createFolderProgramatically.sh
@@ -6,6 +6,9 @@ for file in `ls *.txt`
 do
 	folderName=`echo $file | awk -F. '{print $1}'`
 	echo $folderName
-	mkdir $folderName
-	cp $file $folderName
+	if [ $folderName ]
+	then
+		mkdir $folderName
+		cp $file $folderName
+	fi
 done
