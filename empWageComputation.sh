c566e0ff0363281fca245bd4085b62a9deccf2a0
Mayank <smayank748@gmail.com>
2022-03-11 09:04:17 +0530

    [Mayank]refactored:implemented the attendece check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,11 @@
 #! /bin/bash -x
 
 echo "Welcome to employee wage computation"
+
+randomValue=$((RANDOM))
+if [ $(( $randomValue % 2 )) -eq 1 ]
+then
+	echo "Emp is present"
+else
+	echo "Emp is absent"
+fi
